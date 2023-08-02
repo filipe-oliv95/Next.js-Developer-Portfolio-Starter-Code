cb188fd466dec1a886bb60aef27b43c671cf18a7
filipe-oliv95 <filipec.oliveira95@gmail.com>
2023-08-02 11:51:02 -0300

    add fonts and navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+  return (
+        <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+            <nav>
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+                <Link href="/projects">Projects</Link>
+                <Link href="/articles">Articles</Link>
+            </nav>
+            <nav>
+                <Link href="/" target={"_blank"}>T</Link>
+                <Link href="/" target={"_blank"}>T</Link>
+                <Link href="/" target={"_blank"}>T</Link>
+                <Link href="/" target={"_blank"}>T</Link>
+                <Link href="/" target={"_blank"}>T</Link>
+            </nav>
+        </header>
+    )
+};
+
+export default NavBar;
